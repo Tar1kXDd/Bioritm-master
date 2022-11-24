a9ab60e9a8a578a28bc73082dfd0168be6aa6aae
Tar1kXDd <104137648+Tar1kXDd@users.noreply.github.com>
2022-11-24 20:09:52 +0300

    Задание по биоритмам

---
+++ Bioritm/BIO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioritm
+{
+    public class BIO
+    {
+            
+        public string Date { get; set; }
+        public double Physical { get; set; }
+        public double Emotional { get; set; }
+        public double Intellectual { get; set; }
+        public double Total { get; set; }
+    }
+}
